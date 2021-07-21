020aa87ac4952147f3153714058abf2f7fd6e09e
thomashertbertoh <thomasjackherbert@gmail.com>
2021-07-21 22:50:42 +0200

    plugin functional, added more mobs

--- PowerfulMobs/src/me/Herbert/Thomas/MinecraftPlugins/PowerfulMobs/Listeners/MobSpawnListener.java
+++ PowerfulMobs/src/me/Herbert/Thomas/MinecraftPlugins/PowerfulMobs/Listeners/MobSpawnListener.java
@@ -1,10 +1,13 @@
 package me.Herbert.Thomas.MinecraftPlugins.PowerfulMobs.Listeners;
 
+import org.bukkit.entity.Creeper;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Skeleton;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 
 import me.Herbert.Thomas.MinecraftPlugins.PowerfulMobs.Main.Main;
 
@@ -17,11 +20,37 @@ public class MobSpawnListener implements Listener {
     }
 
     @EventHandler
-    public void mobSpawn(EntitySpawnEvent event) {
-        if (event.getEntity().getType().equals(EntityType.ZOMBIE)) {
-            Zombie z = (Zombie) event.getEntity();
-            z.setBaby();
+    public void creatureSpawn(CreatureSpawnEvent event) {
+        if (!event.getSpawnReason().equals(SpawnReason.CUSTOM)) {
+            EntityType et = event.getEntity().getType();
+            if (et.equals(EntityType.ZOMBIE)) {
+                Zombie z = (Zombie) event.getEntity();
+                z.setBaby();
+            } else if (et.equals(EntityType.SKELETON)) {
+                Skeleton skele = (Skeleton) event.getEntity();
+                event.setCancelled(true);
+                skele.getWorld().spawnEntity(skele.getLocation(), EntityType.STRAY);
+            } else if (et.equals(EntityType.CREEPER)) {
+                Creeper c = (Creeper) event.getEntity();
+                c.setPowered(true);
+            } else if (et.equals(EntityType.SPIDER)) {
+                // TODO: add spider jockeys
+                /*
+                 * CraftWorld cw = (CraftWorld) event.getEntity().getWorld();
+                 * World w = cw.getHandle();
+                 * EntitySpider spider = new EntitySpider(EntityTypes.SPIDER, w);
+                 * EntitySkeleton skeleton = new EntitySkeleton(EntityTypes.SKELETON, w);
+                 * skeleton.spawnIn(w);
+                 * spider.spawnIn(w);
+                 * skeleton.startRiding(spider);
+                 */
+
+                /*
+                 * Spider s = (Spider) event.getEntity();
+                 * Skeleton passenger = (Skeleton) s.getWorld().spawnEntity(s.getLocation(), EntityType.SKELETON);
+                 * s.addPassenger(passenger);
+                 */
+            }
         }
     }
-
 }
--- PowerfulMobs/src/me/Herbert/Thomas/MinecraftPlugins/PowerfulMobs/Main/Main.java
+++ PowerfulMobs/src/me/Herbert/Thomas/MinecraftPlugins/PowerfulMobs/Main/Main.java
@@ -1,12 +1,16 @@
 package me.Herbert.Thomas.MinecraftPlugins.PowerfulMobs.Main;
 
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import me.Herbert.Thomas.MinecraftPlugins.PowerfulMobs.Listeners.MobSpawnListener;
 
 public class Main extends JavaPlugin {
 
     public void onEnable() {
         System.out.println("(!) Enabling PowerfulMobs (!)");
-
+        PluginManager pm = this.getServer().getPluginManager();
+        pm.registerEvents(new MobSpawnListener(this), this);
     }
 
     public void onDisable() {
