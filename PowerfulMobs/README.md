584f0eb52e0762c66c94d913b1b725ca35a28496
thomashertbertoh <thomasjackherbert@gmail.com>
2021-07-22 16:07:36 +0200

    added vexes

--- PowerfulMobs/README.md
+++ PowerfulMobs/README.md
@@ -1,7 +1,7 @@
 ## Version Details
 
-Minecraft version: 1.17.1\
-Plugin version: 1.1
+Minecraft version: 1.16.5\
+Plugin version: 1.2
 
 ## Plugin Details
 
@@ -10,3 +10,4 @@ Changes implemented so far:
 - `zombie`s become `baby zombie`s
 - `skeleton`s become `stray`s
 - `creeper`s spawn charged
+- `bat`s become `vexes` (limit of `20` in loaded chunks to avoid lag)
--- PowerfulMobs/src/me/Herbert/Thomas/MinecraftPlugins/PowerfulMobs/Listeners/MobSpawnListener.java
+++ PowerfulMobs/src/me/Herbert/Thomas/MinecraftPlugins/PowerfulMobs/Listeners/MobSpawnListener.java
@@ -1,6 +1,9 @@
 package me.Herbert.Thomas.MinecraftPlugins.PowerfulMobs.Listeners;
 
+import org.bukkit.Chunk;
+import org.bukkit.entity.Bat;
 import org.bukkit.entity.Creeper;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Skeleton;
 import org.bukkit.entity.Zombie;
@@ -8,6 +11,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.world.ChunkUnloadEvent;
 
 import me.Herbert.Thomas.MinecraftPlugins.PowerfulMobs.Main.Main;
 
@@ -15,42 +20,84 @@ public class MobSpawnListener implements Listener {
 
     Main plugin;
 
+    /*
+     * without some kind of limit you end up with thousands of vexes spawning in a
+     * matter of a couple of minutes, due to bats not counting towards the hostile
+     * mob cap
+     */
+    private int vex_count = 0;
+    private final int VEX_LIMIT = 20;
+
     public MobSpawnListener(Main plugin) {
         this.plugin = plugin;
     }
 
     @EventHandler
     public void creatureSpawn(CreatureSpawnEvent event) {
-        if (!event.getSpawnReason().equals(SpawnReason.CUSTOM)) {
-            EntityType et = event.getEntity().getType();
-            if (et.equals(EntityType.ZOMBIE)) {
+        // mobs spawned by the plugin enter here
+        if (event.getSpawnReason().equals(SpawnReason.CUSTOM)) {
+            return;
+        }
+        switch (event.getEntity().getType()) {
+            case ZOMBIE:
                 Zombie z = (Zombie) event.getEntity();
                 z.setBaby();
-            } else if (et.equals(EntityType.SKELETON)) {
+                break;
+            case SKELETON:
                 Skeleton skele = (Skeleton) event.getEntity();
                 event.setCancelled(true);
                 skele.getWorld().spawnEntity(skele.getLocation(), EntityType.STRAY);
-            } else if (et.equals(EntityType.CREEPER)) {
+                break;
+            case CREEPER:
                 Creeper c = (Creeper) event.getEntity();
                 c.setPowered(true);
-            } else if (et.equals(EntityType.SPIDER)) {
+                break;
+            case SPIDER:
                 // TODO: add spider jockeys
                 /*
-                 * CraftWorld cw = (CraftWorld) event.getEntity().getWorld();
-                 * World w = cw.getHandle();
-                 * EntitySpider spider = new EntitySpider(EntityTypes.SPIDER, w);
-                 * EntitySkeleton skeleton = new EntitySkeleton(EntityTypes.SKELETON, w);
-                 * skeleton.spawnIn(w);
-                 * spider.spawnIn(w);
-                 * skeleton.startRiding(spider);
+                 * CraftWorld cw = (CraftWorld) event.getEntity().getWorld(); World w =
+                 * cw.getHandle(); EntitySpider spider = new EntitySpider(EntityTypes.SPIDER,
+                 * w); EntitySkeleton skeleton = new EntitySkeleton(EntityTypes.SKELETON, w);
+                 * skeleton.spawnIn(w); spider.spawnIn(w); skeleton.startRiding(spider);
                  */
 
                 /*
-                 * Spider s = (Spider) event.getEntity();
-                 * Skeleton passenger = (Skeleton) s.getWorld().spawnEntity(s.getLocation(), EntityType.SKELETON);
+                 * Spider s = (Spider) event.getEntity(); Skeleton passenger = (Skeleton)
+                 * s.getWorld().spawnEntity(s.getLocation(), EntityType.SKELETON);
                  * s.addPassenger(passenger);
                  */
+                break;
+            case BAT:
+                if (this.vex_count < this.VEX_LIMIT) {
+                    this.vex_count++;
+                    Bat b = (Bat) event.getEntity();
+                    event.setCancelled(true);
+                    b.getWorld().spawnEntity(b.getLocation(), EntityType.VEX);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @EventHandler
+    public void creatureDeath(EntityDeathEvent event) {
+        if (event.getEntityType().equals(EntityType.VEX)) {
+            this.vex_count--;
+        }
+    }
+
+    @EventHandler
+    public void unloadChunk(ChunkUnloadEvent event) {
+        Chunk c = event.getChunk();
+        int vexes = 0;
+        for (Chunk ch : c.getWorld().getLoadedChunks()) {
+            for (Entity e : ch.getEntities()) {
+                if (e.getType().equals(EntityType.VEX)) {
+                    vexes++;
+                }
             }
         }
+        this.vex_count = vexes;
     }
 }
